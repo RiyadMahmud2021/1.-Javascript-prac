1d911c8fa1ef6dfa3dcbe97fcf61d4ba6e770383
Riyad Mahmud <79051584+RiyadMahmud2021@users.noreply.github.com>
2021-11-11 22:54:56 +0600

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,169 @@
+// function declaration
+//-----------------------------------------------------------
+//Code:
+ function add(a,b){
+
+     console.log(a+b);     
+
+ }
+ add(5,7);
+//----------------------------------------------------------
+
+
+
+
+
+// global variable and local variable
+//----------------------------------------------------------
+//Code:
+ let username = " Riyad" //global variable because its out of function
+ function helloName(){
+       
+     console.log(" Hello" + username);
+
+ }
+ helloName();
+ console.log(username);
+//---------------------------------------------------------
+
+
+
+
+
+// Parameter and undefined evolution
+//----------------------------------------------------------
+//We can define parameter and argument and We can define argument in parameter too
+//Code:
+ function peraEvolution(a,b){
+     if (typeof a=== "undefined"){
+         a="Riyad"
+     }
+     if (typeof b === "undefined"){
+         b=" Mahmud"
+     }
+     console.log("Hello " + a + b);
+ }
+ peraEvolution();
+//---------------------------------------------------------
+
+
+
+
+
+
+// Returning Value with object declare
+//----------------------------------------------------------
+//Normal or simple function doesn't return value and
+//so that we can't store a value of solving a functional problem  (simple function)
+//if we use return statement we can store value with variable (returning value)
+//Code:
+ function area(a,b){
+    
+     return a*b;
+ }
+
+ area1 = area(5,10); //value storing in a variable 
+ console.log(area1);
+//---------------------------------------------------------
+
+
+
+
+
+// function name setting (get,set,calc,create,check)
+//----------------------------------------------------------
+// When which in need
+//Code:
+ function calcArea(a,b){
+    
+     return a*b; 
+
+ }
+
+ area1 = calcArea(5,110); //storing result in a varibale named "area1"
+ console.log(area1);
+//---------------------------------------------------------
+
+
+
+
+// Dynamic Function
+//-----------------------------------------------------------
+// No named function is stored in a variable
+//Code:
+ var add =function(a,b){
+
+     console.log(a+b);     
+
+ }
+ add(5,7); 
+//----------------------------------------------------------
+
+
+
+
+
+
+// Arrow Function
+//-----------------------------------------------------------
+// With arrow 
+//1. Code:
+  var add = (a,b) => {
+
+     console.log(a+b);     
+
+  }
+  add(5,7);
+//----------------------------------------------------------
+
+
+//-----------------------------------------------------------
+// 
+//2. Code:
+ var hello = (name) => {
+
+     console.log(name);     
+
+  }
+ hello("RIYAD");
+//----------------------------------------------------------
+
+
+//-----------------------------------------------------------
+//  
+//3. Code:
+ var hello = name => {
+     console.log(name);     
+  }
+ hello("RIYAD");
+//----------------------------------------------------------
+
+
+//-----------------------------------------------------------
+//  can give {} for one line
+//4. Code:
+
+ var hello = name => {console.log(name);}
+
+ hello("RIYAD");
+//----------------------------------------------------------
+
+//-----------------------------------------------------------
+//  no need {} for one line
+//5. Code:
+
+ var hello = name => console.log(name);
+
+ hello("RIYAD");
+----------------------------------------------------------
+
+
+//-----------------------------------------------------------
+// need {} for returning function in one line
+//6. Code:
+
+var squre = a => { return a*a ;} ; //same
+//var squre = a =>  a*a ; //same
+
+console.log(squre(25));
+//----------------------------------------------------------
